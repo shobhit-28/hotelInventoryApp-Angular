4ada31adf51484e4a6cafe0ea32065dd30aa12e6
shobhit-28 <shobhitraj34@gmail.com>
2025-04-23 22:30:18 +0530

    basic signin

--- src/app/pages/auth/signin/signin.component.ts
+++ src/app/pages/auth/signin/signin.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { CommonPagesModuleModule } from '../../common-pages-module.module';
+
+type logInType = { userName: string, password: string, [key: string]: string }
 
 @Component({
   selector: 'hinv-signin',
   standalone: true,
-  imports: [],
+  imports: [CommonPagesModuleModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
 })
 export class SigninComponent {
+  private signInDefaultData: logInType = {
+    userName: null!,
+    password: null!,
+  }
 
+  public signInData: logInType = JSON.parse(JSON.stringify(this.signInDefaultData))
+
+  public formsInputFieldsArr: ReadonlyArray<{
+    displayName: string,
+    fieldFormName: string,
+    type: 'text' | 'password'
+  }> = Object.freeze([
+    {
+      displayName: 'User Name',
+      fieldFormName: 'userName',
+      type: 'text',
+    }, {
+      displayName: 'Password',
+      fieldFormName: 'password',
+      type: 'password',
+    }
+  ])
+
+  public logIn(formGroup: NgForm) {
+    console.log(formGroup.form.value)
+    if (formGroup.form.value.userName === 'shobhit' && formGroup.form.value.password === 'admin') {
+      alert('Logged in successfully')
+    } else {
+      alert('Try again later')
+    }
+  }
 }
